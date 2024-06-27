f4e4cc00cc503e37674e340b2e49ddef63b04c48
DPTPhatUS <23125065@student.hcmus.edu.vn>
2024-06-27 18:04:07 +0700

    printDifferences improvements

--- src/utilityFunc.cpp
+++ src/utilityFunc.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-bool printDiffences(std::string s1, std::string s2)
+void printDifferences(std::string s1, std::string s2)
 {
-    if (s1.length() != s2.length())
-        return false;
-    int l = s1.length();
-    bool isSameString = true;
+    std::vector<int> res = findDifferences(s1, s2);
+    if (res.size() == 0)
+    {
+        std::cout << "Same\n";
+        return;
+    }
+    for (int i = 0; i < res.size(); ++i)
+    {
+        std::cout << res[i] << ": ";
+        if (res[i] >= s1.length())
+            std::cout << "null" << " --> " << s2[res[i]] << '\n';
+        else if (res[i] >= s2.length())
+            std::cout << s1[res[i]] << " --> " << "null" << '\n';
+        else
+            std::cout << s1[res[i]] << " --> " << s2[res[i]] << '\n';
+    }
+}
+
+std::vector<int> findDifferences(std::string s1, std::string s2)
+{       
+    if (s1.length() > s2.length())
+        return findDifferences(s2, s1);
+    std::vector<int> res;
+    int l = s2.length();
     for (int i = 0; i < l; ++i)
     {
-        if (s1[i] != s2[i])
-        {
-            std::cout << s1[i] << " --> " << s2[i] << '\n';
-            isSameString = false;
-        }
+        if (i >= s1.length() || s1[i] != s2[i])
+            res.push_back(i);
     }
-    return isSameString;
+    return res;
 }
