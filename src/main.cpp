b0afd6db60911cbc045f77691edab77beaab425e
Phạm Bảo Kha <162149048+MichaelPham2005@users.noreply.github.com>
2024-08-23 16:34:35 +0700

    main for checking tasks

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,56 @@
 
 #include "hashFunc.hpp"
 #include "Nodes.hpp"
-#include "buildMerkleTree.hpp"
+
 int main() {
     std::string myString = "seminars";
-    internalNode* pRoot = buildMerkleTree(myString);
-    std::cout << pRoot -> hash << std::endl;
+    internalNode* pRoot1 = buildMerkleTree(myString);
+    std::cout << pRoot1 -> hash << std::endl;
+
+    //This task is for checking the existance
+    std::cout << "\n1) Checking for the existance" << std::endl;
+    unsigned char targetValues[] = { 'a', 'b', 'c', 'd' }; // 'd' is not in the tree
+    for (unsigned char target : targetValues) {
+        dataNode* foundNode = findDataNode(pRoot1, target);
+
+        if (foundNode) {
+            std::cout << "Found data node with value: " << foundNode->value << std::endl;
+        }
+        else {
+            std::cout << "Data node with value '" << target << "' not found." << std::endl;
+        }
+    }
+
+    // Test generating a Merkle proof for a specific leaf node
+    std::cout << "\n2) Generating Merkle proof path" << std::endl;
+    std::vector<dataNode*> dataVec;
+    for (int i = 0; i < (int)myString.size(); ++i) {
+        dataNode* newData = new dataNode;
+        newData->value = (unsigned char)myString[i];
+        dataVec.push_back(newData);
+    }
+
+    std::vector<leafNode*> leafVec = hashDataToLeaf(dataVec);
+    leafNode* targetLeaf = leafVec[0]; // The leaf node corresponding to 's'
+    std::vector<std::string> proof = generateMerkleProof(targetLeaf);
+
+    std::cout << "Merkle Proof for 's':" << std::endl;
+    for (const std::string& hash : proof) {
+        std::cout << hash << std::endl;
+    }
+
+    //This task is for checking the difference between two Merkle Trees.
+    std::string myString2 = "seninars";
+    internalNode* pRoot2 = buildMerkleTree(myString);
+    std::cout << pRoot2->hash << std::endl;
+
+    if (pRoot1->hash != pRoot2->hash) {
+        std::cout << "=> There might be some difference between two trees." << std::endl;
+        std::vector<std::pair<dataNode*, dataNode*>> res;
+        findDifferences(pRoot1, pRoot2, res);
+        printDifferences(pRoot1, pRoot2);
+    }
+    else std::cout << "=> No difference !" << std::endl;
+
     return 0;
 }
